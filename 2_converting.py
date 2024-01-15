af00ff69a3ef416d16611a00fa6d41435438d4b1
kauevestena <kauemv2@gmail.com>
2024-01-15 16:18:29 +0100

    pmtiles and mbtiles generated

--- 2_converting.py
+++ 2_converting.py
@@ -8,11 +8,12 @@ filename = 'osm_data.gpkg'
 # ogr2ogr -f MVT  "C:\Users\kaue\Documents\tiles" "C:\Users\kaue\Documents\other_footways.parquet" -dsco MAXZOOM=22
 
 # regular_conversion = f'{ogr2ogr_path} -f GPKG test.gpkg {filename} -progress'
-runstring_foldertiles = f'{ogr2ogr_path} -f MVT osm_data_tiles {filename} -dsco MINZOOM=12 -dsco MAXZOOM=20 -progress'
-runstring_mbtiles = f'{ogr2ogr_path} -f MVT tiles osm_data_tiles.mbtiles {filename} -dsco MINZOOM=12 -dsco MAXZOOM=20 -progress'
-runstring_pmtiles = f'{ogr2ogr_path} -f PMTiles tiles osm_data_tiles.pmtiles {filename} -dsco MINZOOM=12 -dsco MAXZOOM=20 -progress'
+# runstring_foldertiles = f'{ogr2ogr_path} -f MVT osm_data_tiles {filename} -dsco MINZOOM=12 -dsco MAXZOOM=20 -progress'
+runstring_mbtiles = f'{ogr2ogr_path} -f MVT osm_data_tiles.mbtiles {filename} -dsco MINZOOM=12 -dsco MAXZOOM=20 -progress'
+runstring_pmtiles = f'{ogr2ogr_path} -f PMTiles osm_data_tiles.pmtiles {filename} -dsco MINZOOM=12 -dsco MAXZOOM=20 -progress'
 
-for runstring in [runstring_foldertiles,runstring_mbtiles, runstring_pmtiles]:
+# for runstring in [runstring_foldertiles,runstring_mbtiles, runstring_pmtiles]:
+for runstring in [runstring_mbtiles, runstring_pmtiles]:
     t1 = time()
     subprocess.run(runstring, shell=True)
     print(f'took {time()-t1} seconds')
