b6a68f8a6300560a38ac41c6b6aad19e4cad8b3d
kauevestena <kauemv2@gmail.com>
2024-01-16 19:46:32 +0100

    another testy test

--- convert_file.py
+++ convert_file.py
@@ -1,9 +1,9 @@
 import subprocess
 
-
+# infos about docker versions: https://github.com/OSGeo/gdal/tree/master/docker
 # ref and thx: https://gis.stackexchange.com/a/420916/49900 
 # docker run --rm -v /home/user/data:/data osgeo/gdal:ubuntu-full-3.4.1 ogr2ogr -f GeoJSON /data/myShapefile.geojson /data/myShapefile.shp
 
 IMG = "ghcr.io/osgeo/gdal:alpine-normal-latest"
 
-runstring = f'docker run --rm -v /home/user/data:/data {IMG} ogr2ogr -f GeoJSON /data/myShapefile.geojson /data/myShapefile.shp'
+runstring = f'docker run --rm -v /data:/data {IMG} ogr2ogr -f GeoJSON /data/myShapefile.geojson /data/myShapefile.shp'
